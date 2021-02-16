a18a41b699e75bc0fc507059a381546112f74f07
welerson-industrycare <welerson.assis@industrycare.com>
2021-02-16 16:09:21 -0300

    botão login

--- core/message.py
+++ core/message.py
@@ -1,38 +1,34 @@
 import requests
 import telegram
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+from core.models import Contact
+import json
 
 TOKEN = '1636613930:AAFSFWtN1wb9ueDP4lZKhLfle8f_IGOOzd4'
 
 bot = telegram.Bot(token=TOKEN)
 
-# def create_user(data):
-#
-#         msg = {
-#             'user_id':user_id,
-#             'first_name':first_name,
-#             'last_name':last_name,
-#         }
-#
-#
-#         print('User ID:{}'.format(user_id))
-#         print('First Name:{}'.format(first_name))
-#         print('Last Name:{}'.format(last_name))
-#         print('Phone Number:{}'.format(phone_number))
-
-def send_message(text, chat_id):
-    url = 'https://api.telegram.org/bot{0}/sendMessage'.format(TOKEN)
-    data = {'chat_id':chat_id, 'text':text}
-    response = requests.post(url, data=data)
-    print(response.content)
+def proccess(data):
+    user =  user_handler(data)
+    login(user, data)
 
 
+def user_handler(data):
+
+        user_id = data['message']['from']['id']
+        first_name = data['message']['from']['first_name']
+        last_name = data['message']['from']['last_name']
+
+        user = {
+            'user_id':user_id,
+            'first_name':first_name,
+            'last_name':last_name,
+        }
+
+        return user
 
 def login(user, data):
-    """
-    Interacts with the contact, requesting your data to authorize access.
-    :param msg: Message from contact
-    """
+
     reply_markup = telegram.ReplyKeyboardMarkup(
         [[telegram.KeyboardButton("Click para Login", request_contact=True)]],
         resize_keyboard=True,
@@ -42,4 +38,23 @@ def login(user, data):
         user["user_id"], "Preciso autorizar seu acesso.", reply_markup=reply_markup
     )
 
-    print(data)
+    user["phone_number"] = data['message']['contact']['phone_number']
+
+    if user['phone_number']:
+         Contact(
+                        user_id=user["user_id"],
+                        first_name=user["first_name"],
+                        last_name=user["last_name"],
+                        phone_number=user["phone_number"],
+                    ).save()
+
+
+
+# def send_message(text, chat_id):
+#     url = 'https://api.telegram.org/bot{0}/sendMessage'.format(TOKEN)
+#     data = {'chat_id':chat_id, 'text':text}
+#     response = requests.post(url, data=data)
+#     print(response.content)
+#
+#
+#
--- core/views.py
+++ core/views.py
@@ -2,29 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
-from core.message import  send_message, login
+from core.message import  proccess
 # Create your views here.
 
 @csrf_exempt
 def event(request):
-
     data = json.loads(request.body)
-    chat_id = data['message']['chat']['id']
-    user_id = data['message']['from']['id']
-    first_name = data['message']['from']['first_name']
-    last_name = data['message']['from']['last_name']
-
-    user = {
-        'user_id':user_id,
-        'first_name':first_name,
-        'last_name':last_name,
-    }
-
-
-    # print(user)
-
-    # send_message('Bem-vindo, por favor digite seu número', chat_id)
-    login(user, data)
-
-
+    proccess(data)
     return HttpResponse()
