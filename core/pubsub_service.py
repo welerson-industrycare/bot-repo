61fc6b27f45f3ed4bcf0b9ac670b26979e9a7684
assis <welerson.assis@industrycare.com.br>
2021-07-23 14:41:21 -0300

    Revisão do pubsub

--- core/pubsub_service.py
+++ core/pubsub_service.py
@@ -16,20 +16,15 @@ def publisher():
     host = os.getenv("ACTIVEMQ_HOST") or "localhost"
     port = os.getenv("ACTIVEMQ_PORT") or 61613
     destination = sys.argv[1:2] or ["/topic/event"]
-    destination = "models_botservice"
+    destination = "/topic/models_botservice"
 
     data = current_service_integrator([Contact])
 
     data = json.dumps(data, indent=2)  
 
     conn = stomp.Connection(host_and_ports = [(host, port)])
-    # conn.start()
     conn.connect(login=user,passcode=password)
-    
     conn.send(destination, data, persistent='true')
-    
-    conn.send(destination, "SHUTDOWN", persistent='true')
-
     conn.disconnect()
 
 def listener():
@@ -38,48 +33,32 @@ def listener():
     password = os.getenv("ACTIVEMQ_PASSWORD") or "password"
     host = os.getenv("ACTIVEMQ_HOST") or "localhost"
     port = os.getenv("ACTIVEMQ_PORT") or 61613
-    destination = sys.argv[1:2] or ["/topic/event"]
-    destination = "models_mailservice"
-    destination2 = "models_reposervice"
-    destination3 = "models_mainservice"
-    destination4 = "models_userauthservice"
+    mailservice = "/topic/models_mailservice"
+    reposervice = "/topic/models_reposervice"
+    mainservice = "/topic/models_mainservice"
+    userauthservice = "/topic/models_userauthservice"
 
     class MyListener(object):
     
         def __init__(self, conn):
             self.conn = conn
-            self.count = 0
-            self.start = time.time()
         
         def on_error(self, message):
             print('received an error %s' % message)
 
         def on_message(self, message):
-            if message.body == "SHUTDOWN":
-        
-                diff = time.time() - self.start
-                print("Received %s in %f seconds" % (self.count, diff))
-                conn.disconnect()
-                sys.exit(0)
+            data_receive = json.loads(message.body)
+            insert_data(data_receive)
             
-            else:
-                if self.count==0:
-                    data_receive = json.loads(message.body)
-                    self.start = time.time()
-                    insert_data(data_receive)
-            
-                self.count += 1
-                if self.count % 2 == 0:
-                    print("Received %s messages." % self.count)
 
     conn = stomp.Connection(host_and_ports = [(host, port)])
     conn.set_listener('', MyListener(conn))
 
-    conn.connect(login=user,passcode=password)
-    conn.subscribe(id='stomp_listener', destination=destination, ack='auto')
-    conn.subscribe(id='stomp_listener', destination=destination2, ack='auto')
-    conn.subscribe(id='stomp_listener', destination=destination3, ack='auto')
-    conn.subscribe(id='stomp_listener', destination=destination4, ack='auto')
+    conn.connect(login=user,passcode=password, wait=True, headers={'client-id':'botClient'})
+    conn.subscribe(id='bot_listener_1', destination=mailservice, ack='auto', headers={'activemq.subscriptionName':'bot_mail'})
+    conn.subscribe(id='bot_listener_2', destination=reposervice, ack='auto', headers={'activemq.subscriptionName':'bot_repo'})
+    conn.subscribe(id='bot_listener_3', destination=mainservice, ack='auto', headers={'activemq.subscriptionName':'bot_main'})
+    conn.subscribe(id='bot_listener_4', destination=userauthservice, ack='auto', headers={'activemq.subscriptionName':'bot_user'})
     
 
     print('sent message')
